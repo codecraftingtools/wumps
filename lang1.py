80c560c2e86cb806516ce96539a461fd8f35bde5
Jeff Webb <jeff.webb@codecraftsmen.org>
2017-10-15 11:58:59 -0500

    Implemented indent/dedent.

--- lang1.py
+++ lang1.py
@@ -2,73 +2,135 @@
 
 from parglare_mod import Parser, Grammar
 import re
-from parglare.parser import default_shift_action
+from parglare.parser import default_shift_action, default_reduce_action
+#from parglare.parser import NodeTerm NodeNonTerm
 
 grammar = r"""
 file: statements EOF;
-statements: statement | statements statement;
-//statement: words endl align |
-//           words ellipsis continuation_li
-statement: words statement_end;
+statements: statement | statements new_line statement
+                      | statements new_line indent statement
+                      | statements new_line dedent statement
+                      | statements new_line dedent dedent statement;
+statement: call_statement;
+call_statement: words;
 words: word | words word;
 word: /[a-zA-Z0-9_]+/;
-statement_end: endl endl;
-endl:; // custom recognizer function
+new_line:; // custom recognizer function
+indent:; // custom recognizer function
+partial_dedent:; // custom recognizer function
+dedent:; // custom recognizer function
 
 LAYOUT: discardables;
 discardables: discardable | discardables discardable;
 discardable: insignificant_spaces | blank_line | EMPTY;
-insignificant_spaces:; // custom recognizer function
-//ws: / +/;
 blank_line: /\n *(?=\n)/;
+insignificant_spaces: / */;
 """
 
-INDENTING = 1
-INSIGNIFICANT = 2
-#ws_mode = INDENTING
-ws_mode = INSIGNIFICANT
-indent_stack = [0]
-insignificant_spaces_re = re.compile(" +")
-def insignificant_spaces_recognizer(input, pos):
-    if ws_mode == INSIGNIFICANT:
-        m = insignificant_spaces_re.match(input, pos)
-        if m:
-            return input[pos:m.end()]
+NEW_LINE = 0
+INDENT = 1
+DEDENT = 2
+PARTIAL_DEDENT = 3
+new_line_token = NEW_LINE
+indent_stack = [""]
+new_indent = ""
 
-endl_re = re.compile("\n")
-endl_count = 0
-def endl_recognizer(input, pos):
-    if input[pos] == "\n":
-        if endl_count == 0:
-            return ""
+new_line_re = re.compile("\n *")
+def new_line_recognizer(input, pos):
+    if new_line_token != NEW_LINE:
+        return None
+    m = new_line_re.match(input, pos)
+    if m:
+        global new_indent
+        new_indent = input[pos+1:m.end()]
+        if len(new_indent) == len(indent_stack[-1]):
+            return input[pos:m.end()]
         else:
-            return "\n"
-    #m = insignificant_spaces_re.match(input, pos)
-    #if m:
-    #    return input[pos:m.end()]
-def endl_action(context, node):
-    global endl_count
-    if endl_count == 0:
-        endl_count = 1
+            return ""
+
+def new_line_action(context, node):
+    global new_line_token
+    if len(new_indent) == len(indent_stack[-1]):
+        new_line_token = NEW_LINE
+    elif len(new_indent) > len(indent_stack[-1]):
+        new_line_token = INDENT
+        indent_stack.append(new_indent)
+    elif len(new_indent) > len(indent_stack[-2]):
+        new_line_token = PARTIAL_DEDENT
+        indent_stack.pop()
+        indent_stack.append(new_indent)
     else:
-        endl_count = 0
-    #endl_count += 1
+        new_line_token = DEDENT
+        indent_stack.pop()
+    return default_shift_action(context, node)
+
+def indent_recognizer(input, pos):
+    if new_line_token != INDENT:
+        return None
+    m = new_line_re.match(input, pos)
+    if m:
+        return input[pos:m.end()]
+
+def indent_action(context, node):
+    global new_line_token
+    new_line_token = NEW_LINE
+    return default_shift_action(context, node)
+
+def partial_dedent_recognizer(input, pos):
+    if new_line_token != PARTIAL_DEDENT:
+        return None
+    m = new_line_re.match(input, pos)
+    if m:
+        return input[pos:m.end()]
+
+def partial_dedent_action(context, node):
+    global new_line_token
+    new_line_token = NEW_LINE
+    return default_shift_action(context, node)
+
+def dedent_recognizer(input, pos):
+    if new_line_token != DEDENT:
+        return None
+    m = new_line_re.match(input, pos)
+    if m:
+        if len(new_indent) == len(indent_stack[-1]):
+            return input[pos:m.end()]
+        else:
+            return ""
+
+def dedent_action(context, node):
+    global new_line_token
+    if len(new_indent) == len(indent_stack[-1]):
+        new_line_token = NEW_LINE
+    elif len(new_indent) > len(indent_stack[-1]):
+        new_line_token = PARTIAL_DEDENT
+        indent_stack.pop()
+        indent_stack.append(new_indent)
+    else:
+        new_line_token = DEDENT
+        indent_stack.pop()
     return default_shift_action(context, node)
 
 recognizers = {
-    'insignificant_spaces': insignificant_spaces_recognizer,
-    'endl': endl_recognizer,
+    'new_line': new_line_recognizer,
+    'indent': indent_recognizer,
+    'partial_dedent': partial_dedent_recognizer,
+    'dedent': dedent_recognizer,
 }
 
 actions = {
-    'endl': endl_action,
+    'new_line': new_line_action,
+    'indent': indent_action,
+    'partial_dedent': partial_dedent_action,
+    'dedent': dedent_action,
 }
 
 g = Grammar.from_string(grammar, recognizers=recognizers)
 parser = Parser(g, actions=actions)
 result = parser.parse(
 """1 a b c
-2 d e
-3 f
-""")
+     2 d e f
+       3 g h i
+4 j k l
+5 m n o""")
 print(result.tree_str())
