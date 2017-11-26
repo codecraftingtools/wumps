6a951f8ea87303f51d5dd6cbffbf2e95de3d5419
Jeff Webb <jeff.webb@codecraftsmen.org>
2017-11-26 04:57:16 -0600

    Implemented line continuations.
    Behavior after line continuation is not working as desired.

--- lang1.py
+++ lang1.py
@@ -6,8 +6,14 @@ import re
 input_string = r"""
 f b \
   a d
-  b c;
-  d e
+  b c ... # comment
+    d e
+  a
+    b
+  a
+    f g
+      h i
+    j k;
 L1 a b c
   # comment 1
    L2 d e f # comment 2
@@ -54,27 +60,34 @@ LAYOUT:
 discardable:
     insignificant_spaces
   | escaped_newline
-  | line_continuation
   | hash_comment
   | hash_comment_line
+  | continuation_line_marker
+  | continuation_line_indent
+  | continuation_line_align
   | blank_line
   | EMPTY;
-line_continuation: ellipsis "\n";
 insignificant_spaces: / */;
 escaped_newline: /\\ *\n/;
-ellipsis: "...";
 hash_comment: / *#.*(?=\n)/;
 hash_comment_line: /\n *#.*(?=\n)/;
+continuation_line_marker:; // custom recognizer function
+continuation_line_indent:; // custom recognizer function
+continuation_line_align:; // custom recognizer function
 blank_line:; // custom recognizer function
 """
 
 class State:
     def __init__(self):
         self.indent_stack = [""]
+        self.starting_continuation = False
+        self.in_continuation = False
 state = State()
 
-new_line_re = re.compile("\n *")
+new_line_re = re.compile(r"\n *")
 def align_recognizer(input, pos):
+    if state.starting_continuation or state.in_continuation:
+        return None
     m = new_line_re.match(input, pos)
     if m:
         new_indent = input[pos+1:m.end()]
@@ -82,17 +95,17 @@ def align_recognizer(input, pos):
             return input[pos:m.end()]
 
 def block_indent_recognizer(input, pos):
-   m = new_line_re.match(input, pos)
-   if m:
-       new_indent = input[pos+1:m.end()]
-       if len(new_indent) > len(state.indent_stack[-1]):
-           return input[pos:m.end()]
-
-def block_indent_action(context, node):
-    state.indent_stack.append(node[1:])
-    return node
+    if state.starting_continuation:
+        return None
+    m = new_line_re.match(input, pos)
+    if m:
+        new_indent = input[pos+1:m.end()]
+        if len(new_indent) > len(state.indent_stack[-1]):
+            return input[pos:m.end()]
 
 def block_dedent_recognizer(input, pos):
+    if state.starting_continuation:
+        return None
     m = new_line_re.match(input, pos)
     if m:
         new_indent = input[pos+1:m.end()]
@@ -103,11 +116,31 @@ def block_dedent_recognizer(input, pos):
             else:
                 return ""
 
-def block_dedent_action(context, node):
-    state.indent_stack.pop()
-    return node
+continuation_line_marker_re = re.compile(r"\.\.\. *(#.*)?(?=\n)")
+def continuation_line_marker_recognizer(input, pos):
+    m = continuation_line_marker_re.match(input, pos)
+    if m:
+        return input[pos:m.end()]
 
-blank_line_re = re.compile("\n *(?=\n)")
+def continuation_line_indent_recognizer(input, pos):
+    if not state.starting_continuation:
+        return None
+    m = new_line_re.match(input, pos)
+    if m:
+        new_indent = input[pos+1:m.end()]
+        if len(new_indent) > len(state.indent_stack[-1]):
+            return input[pos:m.end()]
+
+def continuation_line_align_recognizer(input, pos):
+    if not state.in_continuation:
+        return None
+    m = new_line_re.match(input, pos)
+    if m:
+        new_indent = input[pos+1:m.end()]
+        if len(new_indent) == len(state.indent_stack[-1]):
+            return input[pos:m.end()]
+
+blank_line_re = re.compile(r"\n *(?=\n)")
 def blank_line_recognizer(input, pos):
     m = blank_line_re.match(input, pos)
     if m:
@@ -122,18 +155,45 @@ recognizers = {
     'block_indent': block_indent_recognizer,
     'block_dedent': block_dedent_recognizer,
     'blank_line': blank_line_recognizer,
+    'continuation_line_marker': continuation_line_marker_recognizer,
+    'continuation_line_indent': continuation_line_indent_recognizer,
+    'continuation_line_align': continuation_line_align_recognizer,
 }
 
 class File(list):
-    pass
-
-class Block(list):
     pass
 
 class Call:
     def __init__(self, command, arguments):
         self.command = command
         self.arguments = arguments
+
+class Block(list):
+    pass
+
+def block_indent_action(context, node):
+    state.indent_stack.append(node[1:])
+    if state.in_continuation:
+        state.indent_stack.pop()
+    state.in_continuation = False
+    return node
+
+def block_dedent_action(context, node):
+    if state.in_continuation:
+        state.indent_stack.pop()
+        state.in_continuation = False
+    state.indent_stack.pop()
+    return node
+
+def continuation_line_marker_action(context, node):
+    state.starting_continuation = True
+    return node
+
+def continuation_line_indent_action(context, node):
+    state.starting_continuation = False
+    state.in_continuation = True
+    state.indent_stack.append(node[1:])
+    return node
 
 actions = {
     'file': lambda _, nodes: File(nodes[0]),
@@ -142,6 +202,8 @@ actions = {
     'block': lambda _, nodes: Block(nodes[1]),
     'block_indent': block_indent_action,
     'block_dedent': block_dedent_action,
+    'continuation_line_marker': continuation_line_marker_action,
+    'continuation_line_indent': continuation_line_indent_action,
 }
 
 indent_str = "  "
