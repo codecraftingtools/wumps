ff39d21427c62206056181186e176323e0d21ef7
Jeff Webb <jeff.webb@codecraftsmen.org>
2018-04-22 22:31:25 -0500

    Merged whitespace handling from lang1.py into lang2.py.

--- lang2.py
+++ lang2.py
@@ -132,14 +132,20 @@ class Sequence(Expressions):
         return cls(nodes[1]._expressions, context=context)
 
 input_string = r"""
+if a b c ...
+  then:
+    a1
+    a2
+  else:
+    b
 a
-{b;c}
-a, b, f b (aa,) a : c d e b: d {e}
+#{b;c}
+#a, b, f b (aa,) a : c d e b: d {e}
 """
 #a d: z1 e: z3
 #f2 (b k1: a k2: b c)
 
-grammar = r"""
+grammar_string = r"""
 file:
     expressions
     EOF;
@@ -149,8 +155,8 @@ expressions:
 primary_delimiters:
     primary_delimiter+;
 primary_delimiter:
-    indent | ";";
-indent: "\n";
+    align | ";";
+align:; // custom recognizer function
 expression:
     comma_delimited_sequence |
     non_sequence;
@@ -168,6 +174,7 @@ key: /[a-zA-Z_]+[a-zA-Z0-9_]* *:/;
 anonymous_expression:
     parenthesized_expression |
     braced_block |
+    indented_block |
     call |
     identifier;
 parenthesized_expression:
@@ -175,6 +182,10 @@ parenthesized_expression:
     "(" ")";
 braced_block:
     "{" expressions "}";
+indented_block:
+    block_indent expressions block_dedent;
+block_indent:; // custom recognizer function
+block_dedent:; // custom recognizer function
 call:
     identifier named_argument+ |
     identifier argument named_argument*;
@@ -184,6 +195,7 @@ named_argument:
 argument:
     parenthesized_expression |
     braced_block |
+    indented_block |
     chained_call |
     identifier;
 chained_call:
@@ -192,9 +204,154 @@ chained_call:
 LAYOUT:
     discardable+ | EMPTY;
 discardable:
-    spaces;
-spaces: / +/;
+    insignficant_spaces |
+    escaped_newline |
+    hash_comment |
+    hash_comment_line |
+    continuation_line_marker |
+    continuation_line_indent |
+    continuation_line_dedent |
+    continuation_line_align |
+    blank_line;
+insignficant_spaces: / +/;
+escaped_newline: /\\ *\n/;
+hash_comment: / *#.*(?=\n)/;
+hash_comment_line: /\n *#.*(?=\n)/;
+continuation_line_marker:; // custom recognizer function
+continuation_line_indent:; // custom recognizer function
+continuation_line_dedent:; // custom recognizer function
+continuation_line_align:; // custom recognizer function
+blank_line:; // custom recognizer function
 """
+
+class Whitespace_State:
+    def __init__(self):
+        self._indent_stack = [("", False)]
+        self._starting_continuation = False
+    def current_indent(self):
+        return len(self._indent_stack[-1][0])
+    def previous_indent(self):
+        if len(self._ident_stack > 1):
+            return len(self._indent_stack[-2][0])
+        else:
+            return None
+    def push_indent(self, indent):
+        self._indent_stack.append((indent,self._starting_continuation))
+        self._starting_continuation = False
+    def pop_indent(self):
+        self._indent_stack.pop()
+    def in_continuation(self):
+        return self._indent_stack[-1][1]
+    def starting_continuation(self):
+        return self._starting_continuation
+    def start_continuation(self):
+        self._starting_continuation = True
+
+state = Whitespace_State()
+
+new_line_re = re.compile(r"\n *")
+def align_recognizer(input, pos):
+    if state.starting_continuation() or state.in_continuation():
+        return None
+    m = new_line_re.match(input, pos)
+    if m:
+        new_indent = input[pos+1:m.end()]
+        if len(new_indent) == state.current_indent():
+            return input[pos:m.end()]
+
+def block_indent_recognizer(input, pos):
+    if state.starting_continuation():
+        return None
+    m = new_line_re.match(input, pos)
+    if m:
+        new_indent = input[pos+1:m.end()]
+        if len(new_indent) > state.current_indent():
+            return input[pos:m.end()]
+
+def block_dedent_recognizer(input, pos):
+    if state.starting_continuation() or state.in_continuation():
+        return None
+    m = new_line_re.match(input, pos)
+    if m:
+        new_indent = input[pos+1:m.end()]
+        if len(new_indent) < state.current_indent():
+            return ""
+
+continuation_line_marker_re = re.compile(r"\.\.\. *(#.*)?(?=\n)")
+def continuation_line_marker_recognizer(input, pos):
+    m = continuation_line_marker_re.match(input, pos)
+    if m:
+        return input[pos:m.end()]
+
+def continuation_line_indent_recognizer(input, pos):
+    if not state.starting_continuation():
+        return None
+    m = new_line_re.match(input, pos)
+    if m:
+        new_indent = input[pos+1:m.end()]
+        if len(new_indent) > state.current_indent():
+            return input[pos:m.end()]
+
+def continuation_line_dedent_recognizer(input, pos):
+    if not state.in_continuation():
+        return None
+    m = new_line_re.match(input, pos)
+    if m:
+        new_indent = input[pos+1:m.end()]
+        if len(new_indent) < state.current_indent():
+            return ""
+
+def continuation_line_align_recognizer(input, pos):
+    if not state.in_continuation():
+        return None
+    m = new_line_re.match(input, pos)
+    if m:
+        new_indent = input[pos+1:m.end()]
+        if len(new_indent) == state.current_indent():
+            return input[pos:m.end()]
+
+blank_line_re = re.compile(r"\n *(?=\n)")
+def blank_line_recognizer(input, pos):
+    m = blank_line_re.match(input, pos)
+    if m:
+        return input[pos:m.end()]
+    m = new_line_re.match(input, pos)
+    if m:
+        if m.end() == len(input):
+            return input[pos:m.end()]
+
+recognizers = {
+    'align': align_recognizer,
+    'block_indent': block_indent_recognizer,
+    'block_dedent': block_dedent_recognizer,
+    'blank_line': blank_line_recognizer,
+    'continuation_line_marker': continuation_line_marker_recognizer,
+    'continuation_line_indent': continuation_line_indent_recognizer,
+    'continuation_line_dedent': continuation_line_dedent_recognizer,
+    'continuation_line_align': continuation_line_align_recognizer,
+}
+
+grammar = Grammar.from_string(grammar_string, recognizers=recognizers)
+
+def block_indent_action(context, node):
+    state.push_indent(node[1:])
+    return node
+
+def block_dedent_action(context, node):
+    state.pop_indent()
+    return node
+
+def continuation_line_marker_action(context, node):
+    state.start_continuation()
+    return node
+
+def continuation_line_indent_action(context, node):
+    state.push_indent(node[1:])
+    return node
+
+def continuation_line_dedent_action(context, node):
+    state.pop_indent()
+    return node
 
 actions = {
     'file': File.parse_action,
@@ -205,16 +362,21 @@ actions = {
     'parenthesized_expression':
         lambda context, nodes: nodes[1] if (len(nodes) > 2) else Sequence(),
     'braced_block': Sequence.braced_block_parse_action,
+    'indented_block': Sequence.braced_block_parse_action,
     'call': Call.parse_action,
     'identifier': Identifier.parse_action,
     'named_argument': Named_Expression.parse_action,
     'chained_call': Call.parse_action,
+
+    'block_indent': block_indent_action,
+    'block_dedent': block_dedent_action,
+    'continuation_line_marker': continuation_line_marker_action,
+    'continuation_line_indent': continuation_line_indent_action,
+    'continuation_line_dedent': continuation_line_dedent_action,
 }
 
-g = Grammar.from_string(grammar)
-
-parser = Parser(g, actions=actions,debug=0)
-#parser = GLRParser(g, actions=actions,debug=0)
+parser = Parser(grammar, actions=actions,debug=0)
+#parser = GLRParser(grammar, actions=actions,debug=0)
 
 file = parser.parse(input_string)
 if isinstance(file, list):
