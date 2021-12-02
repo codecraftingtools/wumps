566bdf88b3d7b789b8192bcb38e4a688a2b44a62
Jeff Webb <jeff.webb@codecraftsmen.org>
2021-12-02 14:32:22 -0600

    Added cleanup script

---
+++ wumps/scripts/cleanup_wumps.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Copyright 2021 NTA, Inc.
+
+import os
+
+# Clean up backup and Python cache files from wumps
+if __name__ == "__main__":
+    os.system("find . -name '__pycache__' | xargs rm -rf")
+    os.system("find . -name '*~' | xargs rm -f")
